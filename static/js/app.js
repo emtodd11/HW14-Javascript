b7dc848a3fd396400156e63c1b0212bcd0170aa8
Emily <emilyjtodd1@gmail.com>
2019-04-05 21:00:36 -0800

    part 1 complete
    part 1 complete

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,45 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+let tbody = d3.select("tbody");
+
+// Console.log the data from data.js
+console.table(data);
+
+// create a function that will populate the table with the data
+function buildTable(data) {
+    tbody.html("");
+
+    // run through each object in data and add rows to the table
+    data.forEach((dataRow) => {
+        console.table(dataRow);
+        let row = tbody.append('tr');
+    
+        // add the object values to the table cells
+        Object.values(dataRow).forEach((value) => {
+            let cell = row.append('td');
+            cell.text(value);
+        })
+    })
+}
+
+// create a function after click on filter button
+function handleClick() {
+    d3.event.preventDefault();
+
+    let date = d3.select('#datetime').property('value');
+    let filterData = tableData;
+
+    if(date) {
+        filterData = filterData.filter((row) => row.datetime === date);
+    }
+
+    buildTable(filterData);
+}
+
+// event for button click
+d3.selectAll('#filter-btn').on('click', handleClick);
+
+// call the function
+buildTable(tableData);
